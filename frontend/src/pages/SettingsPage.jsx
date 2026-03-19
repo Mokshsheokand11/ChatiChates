fbeea22383bd41f97c3473ed245911c02b1ed4fc
Mokshsheokand11 <mokshsheokand11@gmail.com>
2026-03-19 23:38:19 +0530

    implement initial sign-up page with form validation and UI, and add linting utility files.

--- frontend/src/pages/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage.jsx
@@ -1,7 +1,7 @@
 
 import { Send } from "lucide-react";
 
-const THEMES = ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter", "dim", "nord", "sunset"];
+const THEMES = ["chatichates", "light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter", "dim", "nord", "sunset"];
 
 const SettingsPage = () => {
     return (
